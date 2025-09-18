391289664eadeb9f3776ee59f2bf04f3b22a0a41
SuryaR03 <147140237+SuryaR03@users.noreply.github.com>
2025-09-18 13:34:03 +0530

    Update GATES_TT.v

--- GATES_TT.v
+++ GATES_TT.v
@@ -3,9 +3,9 @@ input a,b;
 output andgate,orgate,notgate,nandgate,norgate,xorgate,xnorgate;
 and(andgate,a,b);
 or(orgate,a,b);
-// type code for NOT Gate
+not(notgate,a);
 nand(nandgate,a,b);
 nor(norgate,a,b);
-// type code for XOR Gate
-// type code for XNOR Gate
+xor(xorgate,a,b);    
+xnor(xnorgate,a,b); 
 endmodule
